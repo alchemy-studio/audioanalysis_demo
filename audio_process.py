6a59ca7735ad5a2c5855c26064285821457be4ec
breadbread1984 <breadbread1984@163.com>
2021-05-21 16:59:10 +0800

    update

--- audio_process.py
+++ audio_process.py
@@ -12,27 +12,27 @@ class AudioProcess(object):
   def __init__(self,):
     pass
 
-  def get_audio_from_video(self, video_path: str, output: str = None)->None:
+  def from_video(self, video_path: str, output: str = None)->None:
     if output is None:
-      output = splitext(video_path)[0] + '.wav';
+      output = splitext(video_path)[0] + '.flac';
     subprocess.run(['ffmpeg', '-i', video_path, output]);
 
-  def resample_audio(self, audio_path: str, frequency: int = 8000, channels: int = 2, output: str = None)->None:
+  def resample(self, audio_path: str, frequency: int = 8000, channels: int = 2, output: str = None)->None:
     if output is None:
-      output = splitext(audio_path)[0] + "_" + str(frequency) + "_" + ('mono' if channels == 1 else 'stereo') + ".wav";
+      output = splitext(audio_path)[0] + "_" + str(frequency) + "_" + ('mono' if channels == 1 else 'stereo') + ".flac";
     subprocess.run(['ffmpeg', '-i', audio_path, '-ar', frequency, '-ac', channels, output]);
 
-  def show_audio_attributes(self, audio_path: str)->None:
+  def show_attributes(self, audio_path: str)->None:
     subprocess.run(['ffmpeg', '-i', audio_path]);
 
-  def trim_audio(self, audio_path: str, start: int, length: int, output: str = None)->None:
+  def slice(self, audio_path: str, start: int, length: int, output: str = None)->None:
     if output is None:
-      output = splitext(audio_path)[0] + "_trimmed.wav";
+      output = splitext(audio_path)[0] + "_sliced.flac";
     subprocess.run(['ffmpeg', '-i', audio_path, '-ss', start, '-t', length, output]);
 
-  def concat_audio(self, audio_paths: List[str], output: str = None)->None:
+  def concat(self, audio_paths: List[str], output: str = None)->None:
     if output is None:
-      output = 'concated.wav';
+      output = 'concated.flac';
     f = NamedTemporaryFile();
     list_file = f.name;
     for path in audio_path:
@@ -41,9 +41,39 @@ class AudioProcess(object):
     subprocess.run(['ffmpeg', '-f', 'concat', '-i', list_file, '-c', 'copy', output]);
     remove(list_file);
 
-  def split_audio(self, audio_path: str, length: int = 1, output: str = None)->None:
+  def split(self, audio_path: str, length: int = 1, output: str = None)->None:
     if output is None:
-      output = 'splitted%05d.wave';
+      output = splitext(audio_path)[0] + '_splitted%05d.flac';
     subprocess.run(['ffmpeg', '-i', audio_path, '-f', 'segment', '-segment_time', length, '-c', 'copy', output]);
 
-  def 
+  def switch_channels(self, audio_path: str, output: str = None)->None:
+    if output is None:
+      output = splitext(audio_path)[0] + '_switched.flac';
+    subprocess.run(['ffmpeg', '-i', audio_path, '-map_channel', '0.0.1', '-map_channel', '0.0.0', output]);
+
+  def join_channels(self, audio_paths: List[str], output: str = None)->None:
+    if output is None:
+      output = splitext(audio_path)[0] + '_joined.flac';
+    inputs = list();
+    for path in audio_paths:
+      inputs.append('-i');
+      inputs.append(path);
+    subprocess.run(['ffmpeg', *inputs, "-filter_complex", "[0:a][1:a]join=inputs=2:channel_layout=stereo[a]", "-map", "[a]", output]);
+
+  def split_channels(self, audio_path: str, left_output: str = None, right_output: str = None)->None:
+    if left_output is None:
+      left_output = splitext(audio_path)[0] + "_left.flac";
+    if right_output is None:
+      right_output = splitext(audio_path)[0] + "_right.flac";
+    subprocess.run(['ffmpeg', '-i', audio_path, '-map_channel', '0.0.0', left_output, '-map_channel', '0.0.1', right_output]);
+
+  def mute_channels(self, audio_path: str, channel: str = 'left', output: str = None)->None:
+    assert channel in ['left', 'right'];
+    if output is None:
+      output = splitext(audio_path)[0] + "_muted.flac";
+    if channel == 'left':
+      subprocess.run(['ffmpeg', '-i', audio_path, '-map_channel', '-1', '-map_channel', '0.0.1', output]);
+    else:
+      subprocess.run(['ffmpeg', '-i', audio_path, '-map_channel', '0.0.0', output, '-map_channel', '-1']);
+
+
